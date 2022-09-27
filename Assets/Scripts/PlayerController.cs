fbc4346ff94474c0472c7727b7ee8b0d4fc3e7a3
Clarissa2307 <marthaleo07@gmail.com>
2022-09-27 14:40:00 -0500

    Detectar el suelo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
     //variables de movimiento del personaje 
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +23,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump(); 
+
+        }
     }
     void Jump()
     {
+        if(IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+        }
+    }
 
+    // Nos indica si el perosnaje esta tocando el suelo 
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position,
+            Vector2.down,
+                1.5f,
+                groundMask)){
+                //TODO: programar logica de contacto del suelo 
+                    return true;
+                }else{
+                //TODO: programar logica de no contacto 
+                    return false;
+                }
     }
 }

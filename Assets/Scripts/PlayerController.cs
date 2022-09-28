dd06fbd729cf93253ebc17efe02a22afeedee243
Clarissa2307 <marthaleo07@gmail.com>
2022-09-28 17:38:21 -0500

    Caminar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //variables de movimiento del personaje 
     public float jumpForce = 6f;
+    public float runningSpeed = 2f;
     Rigidbody2D rigidBody;
     Animator animator;
 
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundMask;
 
     void Awake()
-    {
+       {
         rigidBody = GetComponent<Rigidbody2D>();
         animator  = GetComponent<Animator>();
     }
@@ -36,6 +37,13 @@ public class PlayerController : MonoBehaviour
         }
         animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
         Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
+    }
+    void FixedUpdate()
+    {
+        if (rigidBody.velocity.x < runningSpeed)
+        {
+            rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+        }
     }
     void Jump()
     {

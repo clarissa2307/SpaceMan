a2c5ae7c02e724f6df47c1ab23415344831e03d1
Clarissa2307 <marthaleo07@gmail.com>
2022-10-13 23:56:01 -0500

    niveles

--- Assets/Scripts/LevelBlock.cs
+++ Assets/Scripts/LevelBlock.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBock : MonoBehaviour
+public class LevelBlock : MonoBehaviour
 {
     public Transform startPoint, exitPoint;
     // Start is called before the first frame update

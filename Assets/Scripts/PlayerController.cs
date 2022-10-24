7270f8829d012e98d5ea9ee8b64a819b0ceda3c0
Clarissa2307 <marthaleo07@gmail.com>
2022-10-23 23:41:05 -0500

    pociones y mana

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,12 @@ public class PlayerController : MonoBehaviour
     const string STATE_ALIVE = "isAlive";
     const string STATE_ON_THE_GROUND = "isOnTheGround";
 
+    private int healthPoints, manaPoints;
+
+    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
+                     MAX_HEALTH = 200, MAX_MANA = 30,
+                     MIN_HEALTH = 10, MIN_MANA = 0;
+
     public LayerMask groundMask;
 
     void Awake()
@@ -32,6 +38,10 @@ public class PlayerController : MonoBehaviour
     {
         animator.SetBool(STATE_ALIVE, true);
         animator.SetBool(STATE_ON_THE_GROUND, true);
+
+        healthPoints = INITIAL_HEALTH;
+        manaPoints = INITIAL_MANA;
+
         Invoke("RestartPosition", 0.1f);
     }
 
@@ -96,5 +106,29 @@ public class PlayerController : MonoBehaviour
     {
         this.animator.SetBool(STATE_ALIVE, false);
         GameManager.sharedInstance.GameOver();
+    }
+    public void CollectHealth(int points)
+    {
+        this.healthPoints += points;
+        if(this.healthPoints >= MAX_HEALTH)
+        {
+            this.healthPoints = MAX_HEALTH;
+        }
+    }
+    public void CollectMana(int points)
+    {
+        this.manaPoints += points;
+        if(this.manaPoints >= MAX_MANA)
+        {
+            this.manaPoints = MAX_MANA;
+        }
+    }
+    public int GetHealth()
+    {
+        return healthPoints;
+    }
+    public int GetMana()
+    {
+        return manaPoints;
     }
 }

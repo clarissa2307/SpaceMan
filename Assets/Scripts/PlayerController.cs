f6903fec7331c96e76e02ca1133abf8d791c0061
Clarissa2307 <marthaleo07@gmail.com>
2022-10-24 20:02:13 -0500

    logica de mana

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
     public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
                      MAX_HEALTH = 200, MAX_MANA = 30,
                      MIN_HEALTH = 10, MIN_MANA = 0;
+    public const int SUPERJUMP_COST = 5;
+    public const float SUPERJUMP_FORCE = 1.5f;
 
     public LayerMask groundMask;
 
@@ -58,9 +60,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if(Input.GetButtonDown("Jump")){
-            Jump(); 
+            Jump(false); 
 
         }
+        if (Input.GetButtonDown("Superjump"))
+        {
+            Jump(true);
+
+        }
+
+
         animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
         Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
     }
@@ -78,13 +87,20 @@ public class PlayerController : MonoBehaviour
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
     }
-    void Jump()
+    void Jump(bool superjump)
     {
+        float jumpForceFactor = jumpForce;
+        if (superjump&&manaPoints>=SUPERJUMP_COST)
+        {
+            manaPoints -= SUPERJUMP_COST;
+            jumpForceFactor *= SUPERJUMP_FORCE;
+        }
+
         if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
             if (IsTouchingTheGround())
             {
-                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                rigidBody.AddForce(Vector2.up * jumpForceFactor, ForceMode2D.Impulse);
             }
         } 
     }

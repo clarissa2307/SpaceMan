8d279938d84d6ba80995cba429486a403cc3897d
Clarissa2307 <marthaleo07@gmail.com>
2022-10-25 22:50:29 -0500

    giro enemigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,8 +22,22 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float currentRunningSpeed = runningSpeed;
+        if (facingRight)
+        {
+            currentRunningSpeed = runningSpeed;
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+        {
+            currentRunningSpeed = -runningSpeed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            rigidBody.velocity = new Vector2(currentRunningSpeed, rigidBody.velocity.y);
+        }
     }
 }
